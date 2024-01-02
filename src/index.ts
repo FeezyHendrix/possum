c56efa6ed0ae0e02360aa776df5d6168014b02a2
Joseph Tsegen <tsegstech10@gmail.com>
2024-01-02 08:33:33 +0100

    feat: doc for configure store

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,39 @@ import {performPossumRequest, processFailedRequestsOnLoad} from "./requests";
 import {configurePossumStore, PossumStoreConfigOptions} from "./storage";
 import {inBrowser} from "./in-browser";
 
-if (inBrowser()) {
-    /**
-     * Adds an event listener to the document that triggers when the DOM is fully loaded.
-     * This ensures that the script runs only after the entire page is loaded.
-     */
-    document.addEventListener("DOMContentLoaded", (_event) => {
-        // Calls the function to process any failed requests that might have been stored.
-        // This is particularly useful for retrying requests that failed in a previous session.
-        try {
-            processFailedRequestsOnLoad();
-        } catch (e) {
-        }
-    })
-
-}
 
 export interface PossumClientOptions {
-    store?: PossumStoreConfigOptions
+    /**
+     * Options to define the storage possum would use to store failed requests.
+     */
+    store?: PossumStoreConfigOptions;
+    /**
+     * If true possum would attach an event listener to process stored failed requests on `DOMContentLoaded`.
+     * This option only works while on the browser
+     */
+    retryOnPageLoad?: boolean;
 }
 
 export function PossumClient(options?: PossumClientOptions) {
     if (options?.store != null) {
         configurePossumStore(options.store)
     }
+
+    if (options?.retryOnPageLoad && inBrowser()) {
+        /**
+         * Adds an event listener to the document that triggers when the DOM is fully loaded.
+         * This ensures that the script runs only after the entire page is loaded.
+         */
+        document.addEventListener("DOMContentLoaded", (_event) => {
+            // Calls the function to process any failed requests that might have been stored.
+            // This is particularly useful for retrying requests that failed in a previous session.
+            try {
+                processFailedRequestsOnLoad();
+            } catch (e) {
+            }
+        })
+    }
+
     return {performPossumRequest, processFailedRequestsOnLoad}
 }
+
--- src/storage.ts
+++ src/storage.ts
@@ -5,8 +5,8 @@ let STORAGE_KEY = "failedRequests";
 
 const inMemoryStore: Record<string, any> = {}
 
-let PossumStore = {
-    get(id: string): any {
+let PossumStore: Pick<PossumStoreConfigOptions, 'get' | 'set'> = {
+    get(id): StoredPossumRequest[] {
         if (!inBrowser()) {
             return inMemoryStore[id]
         }
@@ -15,27 +15,65 @@ let PossumStore = {
 
         return data ? JSON.parse(data) : null
     },
-    set(id: string, data: any): void {
+    set(id, data): void {
         if (!inBrowser()) {
-            inMemoryStore[id] = {...data}
+            inMemoryStore[id] = data
             return
         }
 
-        localStorage.setItem(id, JSON.stringify({...data}));
+        localStorage.setItem(id, JSON.stringify(data));
     }
 }
 
 export interface PossumStoreConfigOptions {
-    /** Name of key to associate with failed requests */
+    /** Name of key to associate with failed requests (Default: `"failedRequests"`) */
     keyName?: string;
     /**
-     * Allows how store retrieves data to be defined
-     * @param id - identifier for failed
+     * Defines how data is retrieved
+     *
+     * @param id - identifier for the stored failed requests
+     *
+     * @example
+     *
+     * import { createClient } from 'redis';
+     *
+     * const redisClient = await createClient()
+     *   .on('error', err => console.log('Redis Client Error', err))
+     *   .connect();
+     *
+     *   //..
+     *   get: async (id: string): Promise<StoredPossumRequest[]> => {
+     *    const data = await redisClient.get(id);
+     *    return data ? JSON.parse(data) : null;
+     *   }
+     *
+     *
+     *
      */
 
-    get: (id: string) => any
-    /** Allows data is stored to be defined */
-    set: (id: string, data: any) => void
+    get: (id: string) => StoredPossumRequest[] | Promise<StoredPossumRequest[]>
+    /**
+     * Defines how data is stored
+     *
+     * @param id - identifier for the stored failed requests
+     * @param data - the list containing the failed requests
+     *
+     * @example
+     *
+     * import { createClient } from 'redis';
+     *
+     * const redisClient = await createClient()
+     *   .on('error', err => console.log('Redis Client Error', err))
+     *   .connect();
+     *
+     * //..
+     *
+     * set: async (id: string, data: StoredPossumRequest[]): Promise<void> => {
+     *  await redisClient.set(id, JSON.stringify(data))
+     * }
+     *
+     */
+    set: (id: string, data: StoredPossumRequest[]) => void | Promise<void>
 }
 
 export function configurePossumStore(options: PossumStoreConfigOptions) {
@@ -52,7 +90,7 @@ export function configurePossumStore(options: PossumStoreConfigOptions) {
  * Stores a failed request in local storage.
  * @param request - The PossumRequest object containing the failed request details.
  */
-export function storeFailedRequest(request: PossumRequest) {
+export function storeFailedRequest(request: PossumRequest): void | Promise<void> {
     // Generate a unique ID for the request based on the current timestamp.
     const id = Date.now().toString();
 
@@ -61,6 +99,17 @@ export function storeFailedRequest(request: PossumRequest) {
 
     // Retrieve any existing failed requests from local storage.
     const failedRequests = getFailedRequests();
+
+    // if the store is asynchronous in nature then it returns a promise
+    if (failedRequests instanceof Promise) {
+        return failedRequests.then((failedReqs) => {
+
+            failedReqs.push(storedRequest)
+
+            return PossumStore.set(STORAGE_KEY, failedReqs)
+        })
+
+    }
 
     // Add the new failed request to the array of existing requests.
     failedRequests.push(storedRequest);
@@ -73,22 +122,31 @@ export function storeFailedRequest(request: PossumRequest) {
  * Removes a failed request from local storage by its ID.
  * @param id - The unique ID of the failed request to be removed.
  */
-export function removeFailedRequest(id: string): void {
+export function removeFailedRequest(id: string): void | Promise<void> {
     // Retrieve the current list of failed requests from local storage.
-    const failedRequests = getFailedRequests();
+    let failedRequests = getFailedRequests();
+
+    if (failedRequests instanceof Promise) {
+        return failedRequests.then((failedReqs) => {
+            // Filter out the request with the given ID.
+            const updatedRequests = failedReqs.filter((req) => req.id !== id)
+            // Update possum store with the new list of failed requests.
+            return PossumStore.set(STORAGE_KEY, updatedRequests)
+        })
+    }
 
     // Filter out the request with the given ID.
     const updatedRequests = failedRequests.filter((req) => req.id !== id);
 
     // Update possum store with the new list of failed requests.
-    PossumStore.set(STORAGE_KEY, JSON.stringify(updatedRequests))
+    PossumStore.set(STORAGE_KEY, updatedRequests)
 }
 
 /**
  * Retrieves the list of failed requests stored in local storage.
- * @returns An array of StoredPossumRequest objects.
+ * @returns An array of StoredPossumRequest objects or a promise of it.
  */
-export function getFailedRequests(): StoredPossumRequest[] {
+export function getFailedRequests(): StoredPossumRequest[] | Promise<StoredPossumRequest[]> {
     // Retrieve the stored failed requests from possum store.
     return PossumStore.get(STORAGE_KEY)
 }
